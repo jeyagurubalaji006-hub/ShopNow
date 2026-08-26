c6c2469b45b431c2927739c69fdd87c939e64baa
JeyaGuruBalaji <jgbalajinfinity006@gmail.com>
2026-08-26 16:21:20 +0530

    Update JwtAuthFilter.java

--- backend/src/main/java/com/shopnow/JWTSecurity/JwtAuthFilter.java
+++ backend/src/main/java/com/shopnow/JWTSecurity/JwtAuthFilter.java
@@ -27,7 +27,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        final String authHeader = request.getHeader("Authorization");
+        String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
